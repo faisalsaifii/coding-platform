df9fbe845ffc5a283a2e3f93139aa8fdc171ea0f
Faisal <m.faisal.s@outlook.com>
2022-09-28 18:35:39 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Editor from './screens/Editor';
 
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="top-pane">
+      <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>
+      <Editor language="css" displayName="CSS" value={css} onChange={setCss}/>
+      <Editor language="javascript" displayName="JS" value={js} onChange={setJs}/>
     </div>
+    <div>
+      <iframe
+        title='output'
+        sandbox='allow-scripts'
+        width='100%'
+        height='100%'
+        frameBorder='0'
+      />
+    </div>
+    </>
   );
 }
 
---
+++ src/screens/Editor.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml' // HTML
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/javascript/javascript'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+function Editor(props) {
+    const { 
+        displayName,
+        language,
+        value,
+        onChange
+    } = props;
+
+    function handleChange ( editor, data, value ) {
+        onChange(value)
+    }
+
+    return (
+        <div>
+            <div>
+                {displayName}
+                <button>
+                    O/C
+                </button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className='code-mirror-wrapper'
+                options={{
+                    // From Codemirror Library
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
+        </div>
+    )
+}
+
+export default Editor
